d617f1094e930705a4b48affeae4fa54df09d589
Mahaboob Raihan <MahaboobRaihan@mahaboob-raihan-mac.local>
2018-04-16 21:49:12 +0100

    collision behaviour, can collide the car with my playerCar

--- 14128577_GameApplication/ViewController.swift
+++ 14128577_GameApplication/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UIViewController {
         }
         sender.setTranslation(CGPoint.zero, in: self.view)
         
+        collisionBehaviour.addBoundary(withIdentifier: "playerCar" as NSCopying, for: UIBezierPath(rect: playerCar.frame))
+        
     }
     
     let carsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
